443e4202b6333070226de330967753fef7882ebc
phchestercl <phchester@gmail.com>
2022-04-26 17:48:52 -0400

    modificacion de ruta del sw 6

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ const STATIC_CACHE='static-v1'
 const INMUTABLE_CACHE='inmutable-v1'
 const DYNAMIC_CACHE='dynamic-v1'
 console.log('Entro en el SW')
-const APP_SHEL=[
+const APP_SHELL=[
     //'/',
     'index.html',
     'css/style.css',
