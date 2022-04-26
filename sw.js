04c677ab79576008186f3490919803a7271d9ef6
phchestercl <phchester@gmail.com>
2022-04-26 17:50:25 -0400

    modificacion de ruta del sw 7

--- sw.js
+++ sw.js
@@ -34,7 +34,6 @@ self.addEventListener('install', e=>{
 
     e.waitUntil(Promise.all([cacheStatic,cacheInmutable]))
 })
-
 self.addEventListener('activate', e =>{
     const eliminacionCachesAntiguos = caches.keys().then(keys => {
         keys.forEach(key =>{
